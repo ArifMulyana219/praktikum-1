36fd7c63cbb73a10324960252ea099f3a50ffcc4
ArifMulyana219 <93023523+ArifMulyana219@users.noreply.github.com>
2021-10-30 14:41:07 +0700

    Praktikum Metode Numerik_Persamaan Non-Linear
    Contoh Solusi Persamaan Non-Linear - Tugas 1 Praktikum Metnum

---
+++ Secant.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import e #untuk memanggil bilangan eksponen natural (e)
+
+#mendefinisikan fungsi 
+def f(x):
+    return e**x-5*x**2
+
+#Metode Secant
+def secant(x0,x1,eps,N):
+    step = 1
+    condition = True
+    while condition :
+        if f(x0) == f(x1):
+            print('Solusi Tidak Ditemukan')
+            break
+        x2 = x1 - ((f(x1)*(x1-x0))/(f(x1)-f(x0)))
+        print('Iterasi-%d,x = %0.8f dan f(x) = %0.8f' % (step,x2,f(x2)))
+        x0 = x1
+        x1 = x2
+        step = step + 1
+
+        if step > N:
+            print('Divergen')
+            break
+        condition = abs(f(x2)) > eps
+    print('/n Akar Persamaan Tersebut : %0.8f' % x2)
+
+#Sesi Input Nilai Awal yang di konversi ke pecahan 
+x0 = float(input('x0 = '))
+x1 = float(input('x1 = '))
+N = float(input('Max_Iter = '))
+eps = float(input('epsilon = '))
+secant(x0,x1,eps,N)
